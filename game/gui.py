3742186ab65410ce2168d63822082ef87d9522ad
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-10-31 18:00:41 +0100

    Add: rotation of tetrominoes.
    - Updated each shape class with angle and rotation attribute to manage all rotation of 90°.
    - Defined method of Tetromino class to check if rotation is valid and to rotate the piece.
    - Added keybord event to allow user to rotate tetrominoes with up arrow.

--- game/gui.py
+++ game/gui.py
@@ -10,18 +10,23 @@ LENGTH = 20
 
 root = tk.Tk()
 main_frame = tk.Frame(root)
-main_frame.pack()
+main_frame.grid(row=0, column=1)
 canvas = tk.Canvas(main_frame, width=500, height=500)
-canvas.pack()
+canvas.grid(row=0, column=2)
 
 tetris = Tetris(canvas)
 
-start_button = tk.Button(main_frame, text='Start', command=tetris.game)
-start_button.pack()
-stop_button = tk.Button(main_frame, text='Reset', command=tetris.reinit)
-stop_button.pack()
+button_frame = tk.Frame(root)
+button_frame.grid(row=0, column=0)
+start_button = tk.Button(button_frame, text='Start', command=tetris.game)
+start_button.grid(row=0, column=0)
+stop_button = tk.Button(button_frame, text='Reset', command=tetris.reinit)
+stop_button.grid(row=1, column=0)
 
-root.bind('<Left>', lambda event,x='left': tetris.tetromino.move(x))
-root.bind('<Right>', lambda event,x='right': tetris.tetromino.move(x))
+
+# Keyboards events
+root.bind('<Left>', lambda event, x='left': tetris.tetromino.move(x))
+root.bind('<Right>', lambda event, x='right': tetris.tetromino.move(x))
+root.bind('<Up>', lambda event: tetris.tetromino.rotate())
 
 root.mainloop()
--- game/shapes.py
+++ game/shapes.py
@@ -1,14 +1,27 @@
+"""Module with classes for shapes."""
+
 from random import randint
 
 
 class ShapeI:
+    """Class for shape I."""
+
     def __init__(self, can):
+        """Initialize shape I."""
         self.length = 4
         self.width = 20
         self.can = can
         self.pieces = []
+        self.angle = 0
+        self.rotation = {
+            0: [(1, -2), (0, -1), (-1, 0), (-2, 1)],
+            90: [(2, 1), (1, 0), (0, -1), (-1, -2)],
+            180: [(-1, 2), (0, 1), (1, 0), (2, -1)],
+            270: [(-2, -1), (-1, 0), (0, 1), (1, 2)]
+        }
 
     def init_piece(self):
+        """Create piece on canvas."""
         init_x = randint(0, 9) * 20
 
         for i in range(self.length):
@@ -23,13 +36,24 @@ class ShapeI:
 
 
 class ShapeL:
+    """Class for shape L."""
+
     def __init__(self, can):
+        """Initialize shape L."""
         self.length = 3
         self.width = 20
         self.can = can
         self.pieces = []
+        self.angle = 0
+        self.rotation = {
+            0: [(0, -2), (-1, -1), (-2, 0), (-1, 1)],
+            90: [(2, 0), (1, -1), (0, -2), (-1, -1)],
+            180: [(0, 2), (1, 1), (2, 0), (1, -1)],
+            270: [(-2, 0), (-1, 1), (0, 2), (1, 1)]
+        }
 
     def init_piece(self):
+        """Create piece on canvas."""
         init_x = randint(0, 8) * 20
         init_y = (-4) * 20
 
@@ -53,13 +77,24 @@ class ShapeL:
 
 
 class ShapeJ:
+    """Class for shape J."""
+
     def __init__(self, can):
+        """Initialize shape J."""
         self.length = 3
         self.width = 20
         self.can = can
         self.pieces = []
+        self.angle = 0
+        self.rotation = {
+            0: [(1, 0), (0, 1), (-1, 2), (-2, 1)],
+            90: [(0, 2), (-1, 1), (-2, 0), (-1, -1)],
+            180: [(-2, 0), (-1, -1), (0, -2), (1, -1)],
+            270: [(0, -2), (1, -1), (2, 0), (1, 1)]
+        }
 
     def init_piece(self):
+        """Create piece on canvas."""
         init_x = randint(1, 9) * 20
         init_y = (-4) * 20
 
@@ -83,13 +118,24 @@ class ShapeJ:
 
 
 class ShapeT:
+    """Class for shape T."""
+
     def __init__(self, can):
+        """Initialize shape T."""
         self.length = 3
         self.width = 20
         self.can = can
         self.pieces = []
+        self.angle = 0
+        self.rotation = {
+            0: [(1, -1), (0, 0), (-1, 1), (-1, -1)],
+            90: [(1, 1), (0, 0), (-1, -1), (1, -1)],
+            180: [(-1, 1), (0, 0), (1, -1), (1, 1)],
+            270: [(-1, -1), (0, 0), (1, 1), (-1, 1)]
+        }
 
     def init_piece(self):
+        """Create piece on canvas."""
         init_x = randint(1, 9) * 20
         init_y = (-4) * 20
 
@@ -114,13 +160,24 @@ class ShapeT:
 
 
 class ShapeZ:
+    """Class for shape Z."""
+
     def __init__(self, can):
+        """Initialize shape Z."""
         self.height = 2
         self.width = 20
         self.can = can
         self.pieces = []
+        self.angle = 0
+        self.rotation = {
+            0: [(0, -2), (1, -1), (0, 0), (1, 1)],
+            90: [(2, 0), (1, 1), (0, 0), (-1, 1)],
+            180: [(0, 2), (-1, 1), (0, 0), (-1, -1)],
+            270: [(-2, 0), (-1, -1), (0, 0), (1, -1)]
+        }
 
     def init_piece(self):
+        """Create piece on canvas."""
         init_x = randint(0, 7) * 20
         init_y = -2 * 20
 
@@ -148,13 +205,24 @@ class ShapeZ:
 
 
 class ShapeS:
+    """Class for shape S."""
+
     def __init__(self, can):
+        """Initialize shape S."""
         self.height = 2
         self.width = 20
         self.can = can
         self.pieces = []
+        self.angle = 0
+        self.rotation = {
+            0: [(2, 0), (1, -1), (0, 0), (-1, -1)],
+            90: [(0, 2), (1, 1),  (0, 0), (1, -1)],
+            180: [(-2, 0), (-1, 1), (0, 0), (1, 1)],
+            270: [(0, -2), (-1, -1), (0, 0), (-1, 1)]
+        }
 
     def init_piece(self):
+        """Create piece on canvas."""
         init_x = randint(2, 9) * 20
         init_y = -2 * 20
 
@@ -182,13 +250,24 @@ class ShapeS:
 
 
 class ShapeSquare:
+    """Class for shape square."""
+
     def __init__(self, can):
+        """Initialize shape square."""
         self.height = 2
         self.width = 20
         self.can = can
         self.pieces = []
+        self.angle = 0
+        self.rotation = {
+            0: [(0, 0), (0, 0), (0, 0), (0, 0)],
+            90: [(0, 0), (0, 0), (0, 0), (0, 0)],
+            180: [(0, 0), (0, 0), (0, 0), (0, 0)],
+            270: [(0, 0), (0, 0), (0, 0), (0, 0)]
+        }
 
     def init_piece(self):
+        """Create piece on canvas."""
         init_x = randint(0, 8) * 20
         init_y = -2 * 20
 
