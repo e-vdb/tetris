06a3ef044b87904f384dfb63772d2dacb5ba6067
Emeline Van der Beken <e.vanderbeken@mediascale.eu>
2022-11-15 08:43:56 +0100

    Add: lines and level in gui.
    - Defined IntValue class in widget_gui module to manage integer variable displayed in frame.
    - Used this class to display score, nb of lines and level in gui.
    - Updated Player class of player module to add lines attributes.
    - Updated Tetris class of tetris module to update lines and level of player.

--- game/player.py
+++ game/player.py
@@ -8,16 +8,22 @@ class Player:
         """Initialize the class."""
         self.score = 0
         self.level = 1
+        self.lines = 0
 
     def reset(self):
         """Reset score and level."""
         self.score = 0
         self.level = 1
+        self.lines = 0
 
     def update_score(self, lines):
         """Update the score."""
         self.score += ((lines * 20) + 10*2**lines) * self.level
-        self.level = self.score // 1000 + 1
+        self.level = self.score // 500 + 1
+
+    def update_lines(self, full_lines):
+        """Update the lines."""
+        self.lines += full_lines
 
     def get_score(self):
         """Return the score."""
